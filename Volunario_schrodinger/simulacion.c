175e60eb6b20c032c37d99aa4c45937d478fc2b7
teresa-p-l <teresapulop@gmail.com>
2025-05-18 10:46:10 +0200

    carpeta creada

---
+++ Volunario_schrodinger/simulacion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+
+// Inicializamos los parámetros
+
+#define N 20        
+#define nc 10.0 //número de ciclos  
+#define h 1.0 
+#define lamba 1.0
+
+#define Time 100.0
+#define PI 3.14159265358979323846
